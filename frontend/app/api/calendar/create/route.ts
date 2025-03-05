0b1019480c70452dd37f361427d50e2ec5aae6af
Nobuhiro Oto <nobuhirooto@gmail.com>
2025-03-05 15:24:36 +0900

    setup routes

---
+++ frontend/app/api/calendar/create/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
+
+ // @ts-ignore: Module 'googleapis' might need to be installed with its types.
+import { google } from "googleapis";
+import { OAuth2Client } from "google-auth-library";
+
+export async function POST() {
+  
+}
